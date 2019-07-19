b9936911d056e43b2b784bb193fdb30af484b7a0
Arthur Goldberg <artgoldberg@gmail.com>
2019-07-18 23:20:14 -0400

    add file to test importing custom IO classes

---
+++ migration_test_repo/migrations/just_writer.py
@@ -0,0 +1,13 @@
+""" Load custom IO classes for migration of model files whose data model is defined by this schema repo
+
+:Author: Arthur Goldberg <Arthur.Goldberg@mssm.edu>
+:Date: 2019-07-18
+:Copyright: 2019, Karr Lab
+:License: MIT
+"""
+
+# if used, this file dynamically loads a Reader and/or Writer from the schema repo
+# if this file exists, it will be imported the obj_model migrator
+# normally (as in wc_lang) this file will import a local Reader and/or Writer
+# but this example just uses obj_model's io
+from obj_model.io import Reader
